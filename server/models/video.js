1667024074da8bcdf5efd839b3ab9aef9e518f10
Raj Manda <rajaram.manda@dharbor.com>
2018-06-04 23:11:56 -0400

    adding password to model

--- server/models/video.js
+++ server/models/video.js
@@ -6,7 +6,8 @@ const videoSchema = new Schema({
     title: String,
     url: String,
     description: String,
-    image: { type: String, required: true }
+    image: { type: String, required: true },
+    password: String
 });
 
 module.exports = mongoose.model('video', videoSchema, 'videos');
